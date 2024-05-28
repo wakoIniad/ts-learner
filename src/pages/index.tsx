a0cd51f45457df4af435d151de35bbb871f593a7
wako_iniad <s1f102400606@iniad.org>
2024-05-28 18:10:52 +0900

    ____xx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,28 +16,20 @@ const Home = () => {
 
 
   function putChecker(x:number,y:number) {
+    if(board[x][y])return false;
     const color = turnColor;
-    /**
-     * + +
-     * + 0
-     * + -
-     * 0 +
-     * 0 -
-     */
-    let result = false;
+    let result = 0;
     for(var i = -1;i <= 1;i ++) {
       for(var j = -1;j <= 1;j ++) {
         if(i || j) {
           let c = 0;//
-          let hasLen = false;
           let [posX,posY] = [x,y];
           while(board[posX+=i] && board[posX][posY+=j]) {
             if(board[posX][posY] === color) {
-              result ||= hasLen;
+              result ||= c;
               break;
             }
             c++;//
-            hasLen=true;
           }
         }
       }
@@ -46,12 +38,16 @@ const Home = () => {
   }
 
   function clicked(x:number,y:number) {
-    if(!board[x][y] && putChecker(x,y)) {
+    if(putChecker(x,y)) {
       const copiedBoard = structuredClone(board);//DeepCopy
       copiedBoard[x][y] = turnColor
       setBoard(copiedBoard)
       setTurnColor(turnColor*-1);
     }
+  }
+
+  function mouseovered() {
+    
   }
 
 
@@ -65,10 +61,11 @@ const Home = () => {
               const row = [];
               for(let y:number=0;y<8;y++) {
                 const stat = board[x][y];
-                let col = stat && ( (stat+1) && 'white'  || 'black' ) || 'transparent';
+                let col = stat && ( (stat+1) && 'white'  || 'black' ) ||
+                putChecker(x,y) && 'rgba(255,255,255,0.5)' || 'transparent';
                 const cell = <div className={styles.cell}>
                   <div className={styles.stonewhite} style={{backgroundColor:col}} key={x+'_'+y}
-                  onClick={ ()=>clicked(x,y) }/>
+                  onClick={ ()=>clicked(x,y) } onMouseOver={ ()=mouseovered(x,y) }/>
                 </div>
                 row.push(cell);
 
